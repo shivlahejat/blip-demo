0d1b63ebef9088bc78c0d24d4c089faf4c0948db
shubhh2903 <shubhamlilawala691@gmail.com>
2026-04-02 15:24:06 +0530

    fix: some change

--- src/imports/NewLandingPage/ui/svgs/SwapSvg.js
+++ src/imports/NewLandingPage/ui/svgs/SwapSvg.js
@@ -9,35 +9,35 @@ const SwapSvg = () => {
       viewBox="0 0 32 32"
       fill="none"
     >
-      <g clip-path="url(#clip0_14516_1758)">
+      <g clipPath="url(#clip0_14516_1758)">
         <path
           d="M14.3365 11.6733C14.3988 11.527 14.4672 11.3819 14.5416 11.2383C16.1307 8.17483 19.9023 6.9796 22.9658 8.56871C26.0293 10.1578 27.2245 13.9294 25.6354 16.9929C24.0463 20.0564 20.2747 21.2516 17.2112 19.6625C17.0677 19.588 16.9282 19.5087 16.7929 19.425"
           stroke="#B35803"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M8.13895 22.5375C5.0755 20.9484 3.88027 17.1768 5.46938 14.1133C7.05849 11.0498 10.8301 9.85462 13.8936 11.4437C16.9571 13.0328 18.1522 16.8044 16.5631 19.8679C14.974 22.9314 11.2025 24.1266 8.13895 22.5375Z"
           fill="#FFBE85"
           stroke="#B35803"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M21.8728 24.2465C21.1393 25.6605 19.3986 26.2121 17.9847 25.4786L16.278 24.5933M16.278 24.5933L18.87 23.7719M16.278 24.5933L17.0994 27.1854"
           stroke="#B35803"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M9.23201 6.85862C9.96543 5.44472 11.7062 4.89309 13.1201 5.62651L14.8268 6.51183M14.8268 6.51183L12.2348 7.33324M14.8268 6.51183L14.0054 3.91978"
           stroke="#B35803"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
